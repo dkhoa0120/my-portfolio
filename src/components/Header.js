ba24db192ebd9bdc8971f8cc3fed7f320b2ebff7
DKHOA111 <dkhoa0120@gmail.com>
2024-03-19 23:15:13 +0700

    complete portfolio

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,12 +4,16 @@ import ThemeToggle from "./ThemeToggle";
 const Header = () => {
   return (
     <div className="p-3 w-[1200px] max-xl:w-full max-sm:w-full flex items-center justify-between h-20">
-      <p className="text-4xl font-mono font-black">
-        <span className="text-[#ec4d37]">K</span>
+      <p className="text-[36px] leading-3 font-line font-mono font-[1000] flex items-center justify-center">
+        <img
+          src="/img/other/logo.png"
+          alt="logo"
+          className="w-[55px] h-[55px]"
+        />
         <span className="dark:text-white">
-          hoa<span className="font-light">Le</span>
+          Khoa<span className="font-light">Le</span>
         </span>{" "}
-        <span className="text-[#ec4d37]">.</span>
+        <span className="text-[#ec4d37] mb-3 ml-1">__</span>
       </p>
       <ThemeToggle />
     </div>
